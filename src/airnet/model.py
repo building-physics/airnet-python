050921e0bfcce5c8da0fffc174c2530c6b0fff3b
Jason W. DeGraw <jason.degraw@gmail.com>
2024-08-01 22:59:44 -0400

    Additional output for regression tests

--- src/airnet/model.py
+++ src/airnet/model.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2023-present Oak Ridge National Laboratory, managed by UT-Battelle
 #
 # SPDX-License-Identifier: BSD-3-Clause
-from __future__ import annotations
+from __future__ import annotations # Remove when dropping 3.9
 import argparse
 import os
 import math
