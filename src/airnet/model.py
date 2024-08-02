6c141e15f4bb84ca23098991daff20febd319c14
Jason W. DeGraw <jason.degraw@gmail.com>
2024-08-01 22:49:59 -0400

    Try to fix 3.8 and 3.9

--- src/airnet/model.py
+++ src/airnet/model.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: 2023-present Oak Ridge National Laboratory, managed by UT-Battelle
 #
 # SPDX-License-Identifier: BSD-3-Clause
+from __future__ import annotations
 import argparse
 import os
 import math
